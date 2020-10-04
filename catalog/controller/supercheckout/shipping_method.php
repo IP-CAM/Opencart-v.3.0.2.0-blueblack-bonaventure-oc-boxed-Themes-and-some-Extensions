87a410483e9bd5e74a3c5a88f92d6019104e5822
freel9242 <71075463+freel9242@users.noreply.github.com>
2020-10-04 16:14:54 +0900

    Update shipping_method.php

--- catalog/controller/supercheckout/shipping_method.php
+++ catalog/controller/supercheckout/shipping_method.php
@@ -244,7 +244,6 @@ class ControllerSupercheckoutShippingMethod extends Controller {
         }
         if (in_array("sameday", array_keys($this->session->data['shipping_methods']))) {
             $data['codeShipping'] = "sameday.sameday";
-            $this->session->data['shipping_method'] = $this->session->data['shipping_methods']['sameday'];
         }
 
         if (isset($this->session->data['comment'])) {
