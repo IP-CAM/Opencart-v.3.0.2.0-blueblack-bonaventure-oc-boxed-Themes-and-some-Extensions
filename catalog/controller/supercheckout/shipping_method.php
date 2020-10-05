110158e5b52d67585c815be6cf1226dfe2f8bd59
freel9242 <71075463+freel9242@users.noreply.github.com>
2020-10-06 01:02:23 +0900

    Update shipping_method.php

--- catalog/controller/supercheckout/shipping_method.php
+++ catalog/controller/supercheckout/shipping_method.php
@@ -155,8 +155,11 @@ class ControllerSupercheckoutShippingMethod extends Controller {
         $this->session->data['shipping_methods'] = array();
         $all_shipping_keys = array_keys($all_shipping);
         foreach ($this->session->data['available_shipping'] as $key => $value) {
-            if (!isset($_POST['country_id']) || !isset($_POST['zone_id']) || !isset($_POST['city_id']) || 
-                !$this->confirm_shipping_method($key, $this->request->post['country_id'], $this->request->post['zone_id'], $this->request->post['city_id'])) continue;
+            if ($this->customer->isLogged() && !$this->confirm_shipping_method($key, $customer_address['country_id'], $customer_address['zone_id'], $customer_address['city_id'])) {
+                continue;
+            } else if (!isset($_POST['country_id']) || !isset($_POST['zone_id']) || !isset($_POST['city_id']) || !$this->confirm_shipping_method($key, $this->request->post['country_id'], $this->request->post['zone_id'], $this->request->post['city_id'])) {
+                continue;
+            }
             if(in_array($key, $all_shipping_keys)){
                 $this->session->data['shipping_methods'][$key] = $all_shipping[$key];
             }
